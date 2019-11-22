60be81fba122897a9489e78e4b4fc0a0277f744d
nadia2612 <YOUR@EMAIL.com>
2019-11-22 22:50:15 +0100

    Step 4. Add a “like” / “dislike” feature to the quotes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 
 export default class Quote extends Component {
+  state = {
+    like: 0
+  };
+  like = () => {
+    this.setState({
+      like: 1
+    });
+  };
+  disLike = () => {
+    this.setState({
+      like: -1
+    });
+  };
+
+  getStyle = () => {
+    if (this.state.like === 1) {
+      return { "font-weight": "bold", color: "green" };
+    } else if (this.state.like === -1) {
+      return { "text-decoration": "line-through", color: "red" };
+    } else {
+      return {};
+    }
+  };
+
   render() {
     const { text, author } = this.props;
+
     return (
       <div className="Quote">
-        <p>{text}</p>
-        <p> By: {author}
-        {/* <button onClick="Like">:)</button>
-        <button onClick="Like">:(</button> */}
+        <p style={this.getStyle()}>{text}</p>
+        <p>
+          By: {author}
+          <button onClick={this.like}>:)</button>
+          <button onClick={this.disLike}>:(</button>
         </p>
       </div>
     );
